8fccb3dcd1a7969e0c304621813e5e33b2537a18
Spy <sornram.spy@gmail.com>
2024-05-10 13:17:56 +0700

    feat:JS

--- ex-3.js
+++ ex-3.js
@@ -13,17 +13,30 @@ const employee = {
   },
 };
 
-console.log("Employee Name: " + employees.name);
+console.log("Employee Name: " + employee.name);
 console.log("Employee Age: " + employee.age);
 console.log(
-  "Employee Address: " +
-    employee.address.street +
+  "Employee Address:",
+  employee["address"]["street"] +
+    ", " +
+    employee["address"]["city"] +
+    ", " +
+    employee["address"]["states"] +
+    ", " +
+    employee["address"]["country"] +
+    " " +
+    employee["address"]["postalCode"]
+);
+
+console.log(
+  "Employee Address:",
+  employee.address.street +
     ", " +
     employee.address.city +
     ", " +
-    employee.address.state +
+    employee.address.states +
     ", " +
     employee.address.country +
     " " +
-    employee.adress.postalCode
+    employee.address.postalCode
 );
