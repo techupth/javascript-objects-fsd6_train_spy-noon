abbc94bc81b7658b5e96397eee79fdef16b8e1c9
trainthunder <kitikun.po@gmail.com>
2024-05-10 12:45:43 +0700

    feat: Train Hackhour

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,14 @@
 // Exercise #1: Food Order
 
 // Start coding here
+
+let foodOrder = {
+  orderNumder: "A0234",
+  address: "Bangkok",
+  paymentType: "Cash",
+  totalPrice: 4500,
+  restarantName: "MK",
+};
+foodOrder.totalPrice = 5000;
+foodOrder.paymentType = "Credit Card";
+console.log(foodOrder);
